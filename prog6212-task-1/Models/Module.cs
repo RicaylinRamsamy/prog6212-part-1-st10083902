ded55d3fad3a2a663a4bd9904f13204e2fdbeb6a
lab_services_student <lab_services_student@lab000000>
2023-09-21 17:37:46 +0000

    Add project files.

---
+++ prog6212-task-1/Models/Module.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace prog6212_task_1.Models
+{
+    class Module
+    {
+        private String code;
+        private String name;
+        private int num_of_credits;
+        private int class_hours_per_week;
+        private int semester_num_of_weeks;
+        private int recommended_self_study_hours_per_week;
+        private Dictionary<DateOnly, int> study_dates = new Dictionary<DateOnly, int>();
+
+        public Module(string code, string name, int num_of_credits, int class_hours_per_week, int semester_num_of_weeks)
+        {
+            this.code = code;
+            this.name = name;
+            this.num_of_credits = num_of_credits;
+            this.class_hours_per_week = class_hours_per_week;
+            this.semester_num_of_weeks = semester_num_of_weeks;
+            recommended_self_study_hours_per_week = ((num_of_credits * 10) / semester_num_of_weeks) - class_hours_per_week;
+        }
+
+        public void setHoursStudied(int hours_studied, DateOnly date)
+        {
+
+            if (study_dates.ContainsKey(date))
+            {
+                study_dates[date] = hours_studied;
+            } else
+            {
+                study_dates.Add(date, hours_studied);
+            }
+        }
+
+        public int getHoursStudiedThisWeek()
+        {
+            DateTime today = DateTime.Today;
+            DateOnly startOfWeek = DateOnly.FromDateTime(today.AddDays(-(int)today.DayOfWeek + 1));
+            DateOnly endOfWeek = startOfWeek.AddDays(6);
+
+            var filtered = study_dates.Where(kvp => kvp.Key >= startOfWeek && kvp.Key <= endOfWeek)
+                         .ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+
+            return filtered.Values.Sum();
+        }
+
+        public int getRemainingSelfStudyHoursThisWeek()
+        {
+            int remaining =  recommended_self_study_hours_per_week - getHoursStudiedThisWeek();
+            return remaining < 0 ? 0 : remaining;
+        }
+    }
+}
---
+++ prog6212-task-1/Windows/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using prog6212_task_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace prog6212_task_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Module module = new Module("prog6212", "Programming 2B", 23, 10, 14);
+            module.setHoursStudied(1, DateOnly.FromDateTime(DateTime.Now));
+/*          module.setHoursStudied(7, new DateOnly(2023, 9, 20));
+            module.setHoursStudied(5, new DateOnly(2023, 9, 17));
+            module.setHoursStudied(4, new DateOnly(2023, 9, 18));
+            module.setHoursStudied(7, new DateOnly(2023, 9, 24));
+            module.setHoursStudied(8, new DateOnly(2023, 9, 20));
+            module.setHoursStudied(10, new DateOnly(2023, 9, 25));*/
+            int hours = module.getHoursStudiedThisWeek();
+            int rem = module.getRemainingSelfStudyHoursThisWeek();
+
+            int x = 3;
+        }
+    }
+}
